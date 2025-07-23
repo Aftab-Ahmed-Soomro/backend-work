e91332233a48c4e2b0895411c934697e0c428824
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2025-07-23 08:56:34 -0700

    backend

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require("cors");
 const cookieParser = require('cookie-parser')
 require("dotenv").config();
-const connectDB = require("./config/db");
+const connectDB = require("./config/db.js");
 const router = require('./routes');
 
 const app = express();
