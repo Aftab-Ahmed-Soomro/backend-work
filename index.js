602725bcbceeb14b9138b70b5a28c51fae2f0576
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2025-07-23 09:03:39 -0700

    sxsd

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
- // To run this on CLI enter cmdline ---> npm run start
-// console.log("Aftab Bhai");
-
 const express = require('express');
 const cors = require("cors");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 require("dotenv").config();
 const connectDB = require("./config/db.js");
 const router = require('./routes');
@@ -16,22 +13,19 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
-app.use("/api",router);
+app.use("/api", router);
 
-const PORT = process.env.PORT;  
+// Avoid calling app.listen() on Vercel
+let isConnected = false;
 
-// 1st Approach (easy)
-
-// connectDB();
-
-// app.listen(PORT,()=> {
-//     console.log("Server is running at : " , PORT);
-// })
-
-// 2nd Approach
-connectDB().then(()=> {
-    app.listen(PORT, ()=> {
+async function handler(req, res) {
+    if (!isConnected) {
+        await connectDB();
+        isConnected = true;
         console.log("DB connected");
-        console.log("Server is running at : ",PORT);
-    })
-})
+    }
+
+    app(req, res);
+}
+
+module.exports = handler;
